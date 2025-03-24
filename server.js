bafe55b5927250acbec44898817f681abfe73e16
srseen <seereen.2540@gmail.com>
2025-03-24 11:18:12 +0700

    remove commented-out router imports and middleware usage from server.js

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const connectDB = require("./Config/db");
 
 require("dotenv").config();
 
-// const authRouter = require("./Routes/auth");
-// const productRouter = require("./Routes/product");
 const app = express();
 connectDB();
 const port = process.env.PORT;
@@ -21,9 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello Server!");
 });
-
-// app.use("/api", authRouter); // http://localhost:3000/api/auth
-// app.use("/api", productRouter); // http://localhost:3000/api/product
 
 readdirSync("./Routes").map((r) => app.use("/api", require(`./Routes/${r}`)));
 
